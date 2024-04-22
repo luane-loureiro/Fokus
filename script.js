82cd88cd0ae81694ae6ea89bfc60f464a9a78b9b
luane-loureiro <luachan@gmail.com>
2024-04-22 16:50:15 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const html = document.querySelector('html');
+
+const focoBt = document.querySelector('.app__card-button--foco');
+const curtoBt = document.querySelector('.app__card-button--curto');
+const longoBt = document.querySelector ('.app__card-button--logo');
+
+focoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'foco')
+})
